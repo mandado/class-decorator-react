4131c48b8b5ad1e8156ac272d60cd7091c7bbf87
Jorge Roberto <jorgerobertodev@gmail.com>
2023-09-28 02:08:09 -0300

    initial committ

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { ClassicComponent, Component, FC, createElement } from "react";
+
+
+type AConstructorTypeOf<T> = new (...args: any[]) => T;
+
+function Render<C extends FC, T extends AConstructorTypeOf<T>>(Comp: C, options?: {
+  inject?: ['state', 'props']
+}) {
+  return function (Klass: T) {
+    // console.log(Klass, Comp);
+    Klass.prototype.render = function () {
+      let props = Object.assign({}, this.state);
+
+      if (options?.inject?.includes('props')) {
+        Object.assign(props, this.props);
+      }
+
+      console.log(Klass.prototype)
+
+      const instanceOnlyMethods = Object.getOwnPropertyNames(Klass.prototype)
+        .filter(prop => prop != "constructor" && prop != "render");
+
+      for (const item of instanceOnlyMethods) {
+        props[item] = Klass.prototype[item].bind(this);
+      }
+      console.log(props)
+      return createElement(Comp, props);
+    }
+
+
+    return new Proxy(Klass, {
+      get(target, prop) {
+        return target[prop as keyof typeof prop];
+      },
+    })
+  };
+}
+
+function ComponentMarkup(props: { count: number, inc: () => void }) {
+  // console.log(props);
+  return (
+    <div className="App">
+      <button onClick={props.inc}>+</button>
+      <h1>{props.count}</h1>
+    </div>
+  );
+}
+
+@Render(ComponentMarkup)
+class Test extends Component<{ count: 1 }> {
+  state = { count: 1 }
+
+  inc() {
+    console.log(this)
+    this.setState({ count: this.state.count + 1 });
+  }
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Test />
+    </div>
+  );
+}
